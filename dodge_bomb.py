05e5cb237410e8f12c7a56ec951cc24773768648
hakozaki <c0a25200fb@edu.teu.ac.jp>
2026-06-16 14:24:30 +0900

    練習問題2:爆弾の作成

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,4 +1,5 @@
 import os
+import random
 import sys
 import pygame as pg
 
@@ -17,11 +18,21 @@ def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("fig/pg_bg.jpg")    
+    # こうかとんの初期化
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+    # 爆弾の初期化
+    bb_img = pg.Surface((20, 20)) 
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10) 
+    bb_img.set_colorkey((0, 0, 0))
+    bb_rct = bb_img.get_rect()
+    bb_rct.centerx = random.randint(0,WIDTH)
+    bb_rct.centery = random.randint(0,HEIGHT)
+    vx, vy = +5, +5
     clock = pg.time.Clock()
     tmr = 0
+    
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
@@ -32,10 +43,12 @@ def main():
         sum_mv = [0, 0]
         for key, mv in DELTA.items():
             if key_lst[key]:
-                sum_mv[0] += mv[0]  #横方向の移動
-                sum_mv[1] += mv[1]  #縦方向の移動 
+                sum_mv[0] += mv[0]  # 横方向の移動
+                sum_mv[1] += mv[1]  # 縦方向の移動 
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip(vx, vy)
+        screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
