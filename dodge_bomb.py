730877eb92705d0fc47d360cd9fc188633985f97
hakozaki <c0a25200fb@edu.teu.ac.jp>
2026-06-16 14:52:35 +0900

    練習問題4:当たり判定

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -52,7 +52,9 @@ def main():
             if event.type == pg.QUIT: 
                 return
         screen.blit(bg_img, [0, 0]) 
-
+        if kk_rct.colliderect(bb_rct):
+            print("ゲームオーバー")
+            return
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
         for key, mv in DELTA.items():
